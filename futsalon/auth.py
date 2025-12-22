cb52a606a7eb1f5e09d62264f180687b6dc2176a
CodeChevalier <110173202+Hydroly@users.noreply.github.com>
2025-12-22 22:03:40 +0330

    Delete futsalon directory

--- futsalon/auth.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Request
-from fastapi.responses import RedirectResponse
-
-def is_logged_in(request: Request):
-    return request.session.get("admin")
-
-def login_required(request: Request):
-    if not is_logged_in(request):
-        return RedirectResponse("/login", status_code=302)
